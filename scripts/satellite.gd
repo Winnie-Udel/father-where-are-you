20ebb86df36aa9d02b057a08432d5d73d201666e
Winnie-Udel <winnie@udel.edu>
2024-03-27 16:49:21 -0400

    Satellite rotates

--- scripts/satellite.gd
+++ scripts/satellite.gd
@@ -2,5 +2,10 @@ extends Enemy
 
 @onready var shot_timer = $ShotTimer
 
+@export var rotation_rate = 20
+
+func _process(delta):
+	rotate(deg_to_rad(rotation_rate) * delta)
+
 func _on_shot_timer_timeout():
 	shoot()
