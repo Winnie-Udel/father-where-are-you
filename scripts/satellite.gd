319d3be6a4ed610add54110fcf7fae7193cbc845
Winnie-Udel <winnie@udel.edu>
2024-03-27 16:51:46 -0400

    Fix satellite rotation

--- scripts/satellite.gd
+++ scripts/satellite.gd
@@ -1,11 +1,12 @@
 extends Enemy
 
 @onready var shot_timer = $ShotTimer
+@onready var satellite_base = $Sprite2D
 
 @export var rotation_rate = 20
 
 func _process(delta):
-	rotate(deg_to_rad(rotation_rate) * delta)
+	satellite_base.rotate(deg_to_rad(rotation_rate) * delta)
 
 func _on_shot_timer_timeout():
 	shoot()
